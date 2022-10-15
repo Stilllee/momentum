70b7bd4810a8c02744ad954f135eb4c3178beec5
wood_stock <108785772+Stilllee@users.noreply.github.com>
2022-10-16 00:40:03 +0900

    .

--- js/clock.js
+++ js/clock.js
@@ -4,8 +4,8 @@ function getClock() {
   const date = new Date();
   const hours = String(date.getHours()).padStart(2, "0");
   const minutes = String(date.getMinutes()).padStart(2, "0");
-  const secounds = String(date.getSeconds()).padStart(2, "0");
-  clock.innerText = `${hours}:${minutes}:${secounds}`;
+  // const secounds = String(date.getSeconds()).padStart(2, "0");
+  clock.innerText = `${hours}:${minutes}`;
 }
 
 getClock();
